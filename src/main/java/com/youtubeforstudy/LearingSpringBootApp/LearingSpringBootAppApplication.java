e34d43ae53132a028d9065a5bc3814017405058e
Sachin kumar <sk4628120@gmail.com>
2025-10-20 23:52:28 +0530

    first commit

---
+++ src/main/java/com/youtubeforstudy/LearingSpringBootApp/LearingSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.youtubeforstudy.LearingSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youtubeforstudy/LearingSpringBootApp/LearingSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubeforstudy.LearingSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearingSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
